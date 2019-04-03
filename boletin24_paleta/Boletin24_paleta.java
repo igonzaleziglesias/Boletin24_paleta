33ae28e678b19c36eadcdfa97c7cedf9194b6586
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2019-04-03 10:28:20 +0200

    boletin24 con paleta

---
+++ boletin24_paleta/Boletin24_paleta.java
@@ -0,0 +1,10 @@
+package boletin24_paleta;
+
+public class Boletin24_paleta {
+
+    public static void main(String[] args) {
+        Paleta obx = new Paleta();
+        obx.setVisible(true);
+    }
+
+}
